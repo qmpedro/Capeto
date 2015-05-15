2a422fcb2c51b16e28540b5234019d2b88613db5
Pedro Q <qmpedro@gmail.com>
2015-05-15 20:14:06 -0300

    Primeiro commit
    Commit inicial

---
+++ PocketMsSql/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace PocketMsSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.txtDataBase = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorkerQuery = new System.ComponentModel.BackgroundWorker();
+            this.progressBarQuery = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHost
+            // 
+            this.txtHost.BackColor = System.Drawing.SystemColors.Window;
+            this.txtHost.Location = new System.Drawing.Point(13, 25);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(191, 20);
+            this.txtHost.TabIndex = 1;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(210, 25);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(106, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(322, 25);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(106, 20);
+            this.txtPass.TabIndex = 3;
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Location = new System.Drawing.Point(12, 165);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.Size = new System.Drawing.Size(637, 254);
+            this.dgvResult.TabIndex = 3;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(12, 136);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(637, 23);
+            this.btnGo.TabIndex = 6;
+            this.btnGo.Text = "G O !";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(13, 51);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(636, 79);
+            this.txtQuery.TabIndex = 5;
+            // 
+            // txtDataBase
+            // 
+            this.txtDataBase.Location = new System.Drawing.Point(434, 25);
+            this.txtDataBase.Name = "txtDataBase";
+            this.txtDataBase.Size = new System.Drawing.Size(215, 20);
+            this.txtDataBase.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "User";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(431, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Database";
+            // 
+            // backgroundWorkerQuery
+            // 
+            this.backgroundWorkerQuery.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerQuery_DoWork);
+            this.backgroundWorkerQuery.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerQuery_RunWorkerCompleted);
+            // 
+            // progressBarQuery
+            // 
+            this.progressBarQuery.Location = new System.Drawing.Point(12, 425);
+            this.progressBarQuery.Name = "progressBarQuery";
+            this.progressBarQuery.Size = new System.Drawing.Size(634, 10);
+            this.progressBarQuery.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBarQuery.TabIndex = 9;
+            this.progressBarQuery.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 439);
+            this.Controls.Add(this.progressBarQuery);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDataBase);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.dgvResult);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtHost);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pocket MS SQL";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.DataGridView dgvResult;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.TextBox txtDataBase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerQuery;
+        private System.Windows.Forms.ProgressBar progressBarQuery;
+    }
+}
+
---
+++ PocketMsSql/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PocketMsSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (!IsValid()) return;
+
+            btnGo.Enabled = false;
+            progressBarQuery.Visible = true;
+            backgroundWorkerQuery.RunWorkerAsync();            
+        }
+
+        private void ShowResult()
+        {
+            try
+            {
+                var strConn = string.Format("Server={0};Database={1};User Id={2};Password={3};", txtHost.Text, txtDataBase.Text, txtUser.Text, txtPass.Text);
+                using (var cnn = new SqlConnection(strConn))
+                {
+                    var cmd = new SqlCommand() { Connection = cnn, CommandType = CommandType.Text, CommandText = txtQuery.Text, CommandTimeout = 600 };
+                    var dt = new DataTable();
+                    var da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+
+                    if (InvokeRequired)
+                        BeginInvoke((MethodInvoker)delegate
+                        {
+                            dgvResult.DataSource = dt;
+                        });
+                }
+            }
+            catch (SqlException ex)
+            {
+                foreach (var erro in ex.Errors)
+                    MessageBox.Show(erro.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool IsValid()
+        {
+            foreach (var control in this.Controls)
+                if (control is TextBox)
+                    SetColorField(((TextBox)control));
+
+            foreach (var control in this.Controls)
+                if (control is TextBox)
+                    if (string.IsNullOrEmpty(((TextBox)control).Text.Trim()))
+                        return false;
+
+            return true;
+        }
+
+        private void SetColorField(TextBox txtBox)
+        {
+            txtBox.BackColor = string.IsNullOrEmpty(txtBox.Text.Trim()) ? Color.LightSalmon : Color.White;
+        }
+
+        private void backgroundWorkerQuery_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ShowResult();
+        }
+
+        private void backgroundWorkerQuery_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBarQuery.Visible = false;
+            btnGo.Enabled = true;
+        }
+    }
+}
